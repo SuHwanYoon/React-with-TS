e1bf7877f6c40a5ae1037148bb11e8e584369bbb
SuHwanYoon <ehtldy@naver.com>
2024-08-11 20:21:50 +0900

    react-with-TS

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1 title='마우스를 올리면 텍스트가 나타납니다'>이것은 <span>타입스크립트</span> 리액트 앱입니다.</h1>//React.createElement
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// 글로벌 스타일 시트를 가져옴
 import './index.css';
+// 앱의 메인 컴포넌트를 가져옴
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+// ReactDOM의 createRoot 메서드를 사용하여 
+// public/index.html 파일의 <div id="root"></div> 요소를 root로 설정함
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement// 'root'라는 id를 가진 DOM 요소를 찾음
 );
+
+// root 요소에 React 컴포넌트를 렌더링함
 root.render(
   <React.StrictMode>
-    <App />
+    <App />{/* App 컴포넌트를 렌더링 */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// 웹 성능을 측정하려면 reportWebVitals에 콜백 함수를 전달하면 됨
+// 예: reportWebVitals(console.log)
 reportWebVitals();
